a0fb63171a2b45f31bf2a07a9115e04fb93eef30
Sreedhar Boggarapu <sreedhar.boggarapu@octoconsulting.com>
2015-10-07 18:36:32 -0400

    Migration process implementation
    Migration process implementation

--- server/boot/migrate-schema.js
+++ server/boot/migrate-schema.js
@@ -1,5 +1,4 @@
 module.exports = function(app) {
-  var models = [];
   var dataSource = app.dataSources.mysql;
   dataSource.autoupdate('DataCenterInformation', function(err) {
     if (err) throw err;
