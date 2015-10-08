e709bf7e05842737207ab7673f2a7793f183339c
Sreedhar Boggarapu <sreedhar.boggarapu@octoconsulting.com>
2015-10-08 11:43:48 -0400

    Fixing the migration processes
    Separating the model sync and migration process

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -7,95 +7,92 @@ var dataSource = app.dataSources.mysql;
 
 var stream = fs.createReadStream("FDCCI DataPoint-inventory.csv");
 
-dataSource.autoupdate('DataCenterInventory', function(err) {
-  var DataCenterInventory = app.models.DataCenterInventory;
-  var dataSet = [];
-  console.log("::::::::::Migration started:::::::::::::");
-  csv.fromStream(stream, {headers: [  
-              'dataCenterId',
-              'agencyDataCenterId',
-              'legacyDataCenterId',
-              'recordStatus',
-              'recordValidity',
-              'agencyAbbreviation',
-              'component',
-              'officeName',
-              'dataCenterName',
-              'publishedName',
-              'coreClassification',
-              'nonCoreSubCategory',
-              'city',
-              'state',
-              'province',
-              'zipCode',
-              'country',
-              'ownershipType',
-              'colocationProvider',
-              'colocationProviderType',
-              'governmentWideProvider',
-              'providingServices',
-              'percentServicesPaidByOtherAgencies',
-              'otherAgenciesServiced',
-              'dataCenterTier',
-              'grossFloorArea',
-              'totalCustomerFloorArea',
-              'annualCostPerSqFt',
-              'fte',
-              'fteCost',
-              'electricityIncludedInCost',
-              'electricityIsMetered',
-              'totalPowerCapacity',
-              'totalITPowerCapacity',
-              'avgElectricityUsage',
-              'avgITElectricityUsage',
-              'costPerKWH',
-              'rackCount',
-              'totalIBMMainframes',
-              'totalOtherMainframes',
-              'totalWindowsServers',
-              'totalUnixServers',
-              'totalLinuxServers',
-              'totalHPCClusterNodes',
-              'otherServers',
-              'totalVirtualHosts',
-              'totalVirtualOS',
-              'totalStorage',
-              'usedStorage',
-              'closingStage',
-              'closingFiscalYear',
-              'closingQuarter',
-              'realPropertyDisposition',
-              'realPropertyDispositionDate',
-              'totalFloorAreaEliminatedOrRepurposed',
-              'totalDecommissionedPhysicalServers',
-              'totalServersMovedToOtherDataCenter',
-              'overallFTEReduction'
-                ]})
-    .on("data", function(data){
-        dataSet.push(data);
-     })
-     .on("end", function(){
-        var count = dataSet.length;
-        if(count == 0){
-          dataSource.disconnect();
-          console.log(":::::::::::::Migration is completed:::::::::");
-        }else{
-          dataSet.splice(0,1);
-          count = dataSet.length
-        }
+var DataCenterInventory = app.models.DataCenterInventory;
+var dataSet = [];
+console.log("::::::::::Migration started:::::::::::::");
+csv.fromStream(stream, {headers: [  
+            'dataCenterId',
+            'agencyDataCenterId',
+            'legacyDataCenterId',
+            'recordStatus',
+            'recordValidity',
+            'agencyAbbreviation',
+            'component',
+            'officeName',
+            'dataCenterName',
+            'publishedName',
+            'coreClassification',
+            'nonCoreSubCategory',
+            'city',
+            'state',
+            'province',
+            'zipCode',
+            'country',
+            'ownershipType',
+            'colocationProvider',
+            'colocationProviderType',
+            'governmentWideProvider',
+            'providingServices',
+            'percentServicesPaidByOtherAgencies',
+            'otherAgenciesServiced',
+            'dataCenterTier',
+            'grossFloorArea',
+            'totalCustomerFloorArea',
+            'annualCostPerSqFt',
+            'fte',
+            'fteCost',
+            'electricityIncludedInCost',
+            'electricityIsMetered',
+            'totalPowerCapacity',
+            'totalITPowerCapacity',
+            'avgElectricityUsage',
+            'avgITElectricityUsage',
+            'costPerKWH',
+            'rackCount',
+            'totalIBMMainframes',
+            'totalOtherMainframes',
+            'totalWindowsServers',
+            'totalUnixServers',
+            'totalLinuxServers',
+            'totalHPCClusterNodes',
+            'otherServers',
+            'totalVirtualHosts',
+            'totalVirtualOS',
+            'totalStorage',
+            'usedStorage',
+            'closingStage',
+            'closingFiscalYear',
+            'closingQuarter',
+            'realPropertyDisposition',
+            'realPropertyDispositionDate',
+            'totalFloorAreaEliminatedOrRepurposed',
+            'totalDecommissionedPhysicalServers',
+            'totalServersMovedToOtherDataCenter',
+            'overallFTEReduction'
+              ]})
+  .on("data", function(data){
+      dataSet.push(data);
+   })
+   .on("end", function(){
+      var count = dataSet.length;
+      if(count == 0){
+        dataSource.disconnect();
+        console.log(":::::::::::::Migration is completed:::::::::");
+      }else{
+        dataSet.splice(0,1);
+        count = dataSet.length;
+      }
 
-        dataSet.forEach(function(data) {         
-          DataCenterInventory.create(data, function(err, record) {
-            count--;
-            if (count === 0) {
-              dataSource.disconnect();
-              console.log(":::::::::::::Migration is completed:::::::::");
-            }
-            if (err) {
-              console.log(data);
-              return console.log(err);
-            }
-          });
+      dataSet.forEach(function(data) {         
+        DataCenterInventory.create(data, function(err, record) {
+          count--;
+          if (count === 0) {
+            dataSource.disconnect();
+            console.log(":::::::::::::Migration is completed:::::::::");
+          }
+          if (err) {
+            return console.log(err);
+          }
         });
-     });
-});
+      });
+   });
