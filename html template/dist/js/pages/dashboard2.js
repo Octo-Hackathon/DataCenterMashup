bbf991f1e44b41cca1b8546ccd724cbcc3532ce2
scottastanton <scotta.stanton@gmail.com>
2015-10-14 16:55:46 -0400

    Updating JS for decommissioned servers

--- html template/dist/js/pages/dashboard2.js
+++ html template/dist/js/pages/dashboard2.js
@@ -131,6 +131,72 @@ $(function () {
   //- END PIE CHART 1 -
   //-----------------
 
+  //-------------
+  //- PIE CHART 2 -
+  //-------------
+  // Get context with jQuery - using jQuery's .get() method.
+  var pieChart2Canvas = $("#pieChart2").get(0).getContext("2d");
+  var pieChart2 = new Chart(pieChart2Canvas);
+  var PieData2 = [
+    {
+      value: 20,
+      color: "#f56954",
+      highlight: "#f56954",
+      label: "Windows Servers"
+    },
+    {
+      value: 11,
+      color: "#00a65a",
+      highlight: "#00a65a",
+      label: "Linux Servers"
+    },
+    {
+      value: 5,
+      color: "#f39c12",
+      highlight: "#f39c12",
+      label: "Unix Servers"
+    },
+    {
+      value: 1,
+      color: "#00c0ef",
+      highlight: "#00c0ef",
+      label: "Other Servers"
+    }        
+  ];
+  var pieOptions2 = {
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke: true,
+    //String - The colour of each segment stroke
+    segmentStrokeColor: "#fff",
+    //Number - The width of each segment stroke
+    segmentStrokeWidth: 1,
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout: 50, // This is 0 for Pie charts
+    //Number - Amount of animation steps
+    animationSteps: 100,
+    //String - Animation easing effect
+    animationEasing: "easeOutBounce",
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate: true,
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale: false,
+    //Boolean - whether to make the chart responsive to window resizing
+    responsive: true,
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio: false,
+    //String - A legend template
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+    //String - A tooltip template
+    tooltipTemplate: "<%=value %> <%=label%>"
+  };
+  //Create pie or douhnut chart
+  // You can switch between pie and douhnut using the method below.
+  pieChart2.Doughnut(PieData2, pieOptions2);
+  //-----------------
+  //- END PIE CHART 2 -
+  //-----------------
+
+
   /* jVector Maps
    * ------------
    * Create a world map with markers
