ad0766899f818fcd0af95fd16790f1bf58790cc6
scottastanton <scotta.stanton@gmail.com>
2015-10-16 15:40:51 -0400

    Changes to main and header

--- client/src/app/views/main/main.controller.js
+++ client/src/app/views/main/main.controller.js
@@ -5,8 +5,9 @@
 			$scope.$state = $state;
 		})
 
-    .controller('MainController', /** @ngInject */ function ($scope, $filter, quarterChartData, serverChartData, storageChartData, allQuarterChartData, fteChartData, electricityChartData) {
+    .controller('MainController', /** @ngInject */ function ($scope, user, $filter, quarterChartData, serverChartData, storageChartData, allQuarterChartData, fteChartData, electricityChartData) {
 		  
+	$scope.user = user.getUser();
 			/* Main Cost Chart */
 
 		  $scope.mainChart = {};
