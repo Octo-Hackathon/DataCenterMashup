9dd993133f0fb09211ce2a9c83c3987c9ccac32e
Dave Reed <dave@contagious.ly>
2015-10-16 09:32:46 -0400

    Added chart.js library and sample chart for dashboard

--- client/src/app/views/main/main.controller.js
+++ client/src/app/views/main/main.controller.js
@@ -1,8 +1,24 @@
 (function() {
 
   angular.module('octoDatacenter')
-    .controller('MainController', /** @ngInject */ function () {
+    .controller('MainController', /** @ngInject */ function ($scope, $filter) {
+		  $scope.labels = ["Q1 2014", "Q2 2014", "Q3 2015", "Q4 2015"];
+		  $scope.data = [
+		    [123456, 234567, 345367, 232222]
+		  ];
 
+			$scope.colors = [{
+				fillColor: 'rgba(60,141,188,0.7)',
+				strokeColor: 'rgba(60,141,188,0.9)',
+				highlightFill: 'rgba(60,141,188,0.9)',
+				highlightStroke: 'rgba(60,141,188,0.9)'
+			}];
+
+		  $scope.options = {
+		  	scaleLabel: function (valuePayload) {
+			    return $filter('currency')(valuePayload.value, '$', 0);
+			}
+		  }
     });
 
 })();
