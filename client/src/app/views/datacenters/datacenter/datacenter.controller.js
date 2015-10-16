a786ac01629fcd9d6d2c3593460cc4038b2d7e13
Dave Reed <dave@contagious.ly>
2015-10-16 09:44:54 -0400

    Updated states list

--- client/src/app/views/datacenters/datacenter/datacenter.controller.js
+++ client/src/app/views/datacenters/datacenter/datacenter.controller.js
@@ -30,6 +30,8 @@
 
 		vm.model = {};
 
+		vm.statesList = [{"name":"Alabama","value":"AL"},{"name":"Alaska","value":"AK"},{"name":"American Samoa","value":"AS"},{"name":"Arizona","value":"AZ"},{"name":"Arkansas","value":"AR"},{"name":"California","value":"CA"},{"name":"Colorado","value":"CO"},{"name":"Connecticut","value":"CT"},{"name":"Delaware","value":"DE"},{"name":"District Of Columbia","value":"DC"},{"name":"Federated States Of Micronesia","value":"FM"},{"name":"Florida","value":"FL"},{"name":"Georgia","value":"GA"},{"name":"Guam","value":"GU"},{"name":"Hawaii","value":"HI"},{"name":"Idaho","value":"ID"},{"name":"Illinois","value":"IL"},{"name":"Indiana","value":"IN"},{"name":"Iowa","value":"IA"},{"name":"Kansas","value":"KS"},{"name":"Kentucky","value":"KY"},{"name":"Louisiana","value":"LA"},{"name":"Maine","value":"ME"},{"name":"Marshall Islands","value":"MH"},{"name":"Maryland","value":"MD"},{"name":"Massachusetts","value":"MA"},{"name":"Michigan","value":"MI"},{"name":"Minnesota","value":"MN"},{"name":"Mississippi","value":"MS"},{"name":"Missouri","value":"MO"},{"name":"Montana","value":"MT"},{"name":"Nebraska","value":"NE"},{"name":"Nevada","value":"NV"},{"name":"New Hampshire","value":"NH"},{"name":"New Jersey","value":"NJ"},{"name":"New Mexico","value":"NM"},{"name":"New York","value":"NY"},{"name":"North Carolina","value":"NC"},{"name":"North Dakota","value":"ND"},{"name":"Northern Mariana Islands","value":"MP"},{"name":"Ohio","value":"OH"},{"name":"Oklahoma","value":"OK"},{"name":"Oregon","value":"OR"},{"name":"Palau","value":"PW"},{"name":"Pennsylvania","value":"PA"},{"name":"Puerto Rico","value":"PR"},{"name":"Rhode Island","value":"RI"},{"name":"South Carolina","value":"SC"},{"name":"South Dakota","value":"SD"},{"name":"Tennessee","value":"TN"},{"name":"Texas","value":"TX"},{"name":"Utah","value":"UT"},{"name":"Vermont","value":"VT"},{"name":"Virgin Islands","value":"VI"},{"name":"Virginia","value":"VA"},{"name":"Washington","value":"WA"},{"name":"West Virginia","value":"WV"},{"name":"Wisconsin","value":"WI"},{"name":"Wyoming","value":"WY"}];
+
 		// Use real data if the service provides it - otherwise show sample data to avoid data entry
 		if(dataCenterInv) {
 			vm.model = Restangular.stripRestangular(dataCenterInv)
@@ -206,11 +208,12 @@
 					},
 					{
 						className: 'col-sm-4',
-						type: 'input',
+						type: 'select',
 						key: 'state',
 						templateOptions: {
 							label: 'State',
-							className: 'form-control'
+							className: 'form-control',
+							options : vm.statesList 
 						}
 					},
 					{
--- client/src/app/views/main/main.controller.js
+++ client/src/app/views/main/main.controller.js
@@ -4,7 +4,7 @@
     .controller('MainController', /** @ngInject */ function ($scope, $filter) {
 		  $scope.labels = ["Q1 2014", "Q2 2014", "Q3 2015", "Q4 2015"];
 		  $scope.data = [
-		    [123456, 234567, 345367, 232222]
+		    [333444, 234567, 345367, 232222]
 		  ];
 
 			$scope.colors = [{
@@ -17,7 +17,7 @@
 		  $scope.options = {
 		  	scaleLabel: function (valuePayload) {
 			    return $filter('currency')(valuePayload.value, '$', 0);
-			}
+				}
 		  }
     });
 
